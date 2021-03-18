07906ece51b2899d22ee7464b60b30a8ed005952
shimosawa <shimon101018@gmail.com>
2021-03-18 22:03:31 +0900

    main func

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// このメイン関数はflutterアプリの開始地点
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('hello world'),
+      ),
+    ),
+  );
+}
