1091ae653763837bb87d5f222bd522d252909a4a
shimosawa <shimon101018@gmail.com>
2021-03-18 22:44:21 +0900

    center widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text('I am rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Image(
+        home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I am rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
           image: NetworkImage(
               'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR9qDoSJLOwRa02yAPE4aN6SVpRaTC0cDoAw&usqp=CAU'),
         ),
       ),
-    ),
+    )),
   );
 }
