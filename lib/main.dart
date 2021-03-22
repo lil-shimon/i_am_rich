73b61b0872a792238d6a77d651365d08fe6f081e
shimosawa <shimon101018@gmail.com>
2021-03-22 20:16:21 +0900

    assetImages

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Text('I am rich'),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      body: Center(
-        child: Image(
-          image: NetworkImage(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR9qDoSJLOwRa02yAPE4aN6SVpRaTC0cDoAw&usqp=CAU'),
+  runApp(MaterialApp(
+    home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
         ),
-      ),
-    )),
-  );
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        )),
+  ));
 }
